c3ef355e8f95a2fa054bebc57f7f5c0ec28a9a1d
Danijel <dvincija@foi.hr>
2015-11-05 21:13:52 +0100

    Kopija privatnog repozitorija
    Prebacivanje cjelokupne dokumentacije sa privatnog repozitorija na
    službeni repozitorij

---
+++ Discount Locator/Discount Locator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Discount Locator
+//
+//  Created by MTLab on 04/11/15.
+//  Copyright © 2015 air. All rights reserved.
+//
+
+import UIKit
+import db
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        //DbController.sharedDBInstance.addDiscount(remoteId: 1, name: "povoljnio sir", store: store)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Discount Locator/db/DbController.swift
@@ -0,0 +1,39 @@
+//
+//  DbController.swift
+//  Discount Locator
+//
+//  Created by MTLab on 04/11/15.
+//  Copyright © 2015 air. All rights reserved.
+//
+
+import Foundation
+
+import RealmSwift
+
+public class DbController
+{
+    public static var sharedDBInstance = DbController()
+    
+    private let realm = try! Realm()
+    
+    public func addStore(remoteId: Int, name: String, desc: String, imgUrl: String, longitude: Int, latitude: Int)
+    {
+        let store = Store()
+        store.remoteId = remoteId
+        store.name = name
+        store.desc = desc
+        store.imgUrl = imgUrl
+        store.longitude = longitude
+        store.latitude = latitude
+        
+        try! self.realm.write
+        {
+            self.realm.add(store)
+        }
+    }
+    
+    public func addDiscount()
+    {
+        
+    }
+}
