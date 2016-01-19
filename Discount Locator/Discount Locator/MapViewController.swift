fcbd441b4e0ccc9e45008089501bfb920007d4cd
Darijan <dvertovs@foi.hr>
2016-01-19 11:39:29 +0100

    map view

--- Discount Locator/Discount Locator/MapViewController.swift
+++ Discount Locator/Discount Locator/MapViewController.swift
@@ -48,13 +48,14 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         bannerView.hidden = true
         
     }
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) { //centrira regiju ovisno o tvojoj poziciji, još ne treba
-//        let location = locations.last! as CLLocation
-//        
-//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-//        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        //self.mapView.setRegion(region, animated: true)
+        let location = locations.last! as CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
     }
 
     func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
